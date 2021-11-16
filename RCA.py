85d68384b6a8e3ff559b8f57e12c0d69a27bbeb5
willrobertshaw <uceswro.ucl.ac.uk>
2021-11-16 20:04:09 +0000

    Feat: rca pseudo

---
+++ RCA.py
@@ -0,0 +1,8 @@
+# SET counting to 0
+# FOR each line of polygon:
+#     IF ray crosses the line:
+#         THEN INC counting by 1
+# IF counting is odd:
+#     THEN the point is inside
+# ELSE
+#     THEN the point is outside
